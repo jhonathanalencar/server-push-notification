675db315a8b4d27507ab0ed1dfdad1a81b9e8b29
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-08-07 16:31:33 -0300

    doc: add frontend usage

--- src/routes/notifications-routes.ts
+++ src/routes/notifications-routes.ts
@@ -43,3 +43,37 @@ export async function notificationsRoutes(app: FastifyInstance) {
     return reply.status(201).send();
   });
 }
+
+// frontend usage
+
+// navigator.serviceWorker
+//   .register('service-worker.js')
+//   .then(async (serviceWorker) => {
+//     let subscription = await serviceWorker.pushManager.getSubscription();
+
+//     if (!subscription) {
+//       const publicKeyResponse = await fetch(
+//         'http://localhost:3333/push/public_key'
+//       );
+//       const publicKeyData = await publicKeyResponse.json();
+
+//       subscription = await serviceWorker.pushManager.subscribe({
+//         userVisibleOnly: true,
+//         applicationServerKey: publicKeyData.publicKey,
+//       });
+//     }
+
+//     await fetch('http://localhost:3333/push/register', {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         subscription,
+//       }),
+//     });
+
+//     await fetch('http://localhost:3333/push/send', {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         subscription,
+//       }),
+//     });
+//   });
